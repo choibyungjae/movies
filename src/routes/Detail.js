3abfc31565a0b9060e85f577561a349bdb575ff7
choibyungjae <shapa355@naver.com>
2022-01-04 20:35:22 +0900

    2022.01.04
    react web movie service

---
+++ src/routes/Detail.js
@@ -0,0 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setMovies(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <h2>{movies.data.movie.title}</h2>
+          <p>{movies.data.movie.description_full}</p>
+          <ul>
+            {movies.data.movie.genres &&
+              movies.data.movie.genres.map((g) => <li key={g}>{g}</li>)}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
